eb0afee58f22b64bf1f831d5252154155cbf56fd
LookerKy <nagaraz07@gmail.com>
2021-04-13 14:06:27 +0900

    login done

--- root/settings.py
+++ root/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
 
     'widget_tweaks',
 
-    'board'
+    'board',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- root/urls.py
+++ root/urls.py
@@ -18,10 +18,12 @@ from django.urls import path
 from django.conf.urls import url
 
 from board import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic')
     # path('', views.home),
